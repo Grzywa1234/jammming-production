55b6ab2699f273395ee30cde7bae51520d589a0e
Grzywa1234 <47593899+Grzywa1234@users.noreply.github.com>
2020-09-30 13:58:13 +0200

    first commit / working app

---
+++ precache-manifest.19be1355831d0a90730f28f66aa358d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96f97c39a6a9abbaa388a78c77422319",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f70a34fc26ee26ddc76",
+    "url": "/static/css/main.3cb3bf77.chunk.css"
+  },
+  {
+    "revision": "b0f597be5bf5280f10d7",
+    "url": "/static/js/2.87f56ebd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.87f56ebd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f70a34fc26ee26ddc76",
+    "url": "/static/js/main.99377c8e.chunk.js"
+  },
+  {
+    "revision": "505e4dc665c09938bf7e",
+    "url": "/static/js/runtime-main.9075e973.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
